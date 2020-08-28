cabefba9e448ff9da124fc7da6db2497ac394cc6
marshallpowell97 <marshallpowell97@gmail.com>
2020-08-27 21:12:29 -0400

    Add database for members

--- main.py
+++ main.py
@@ -1,9 +1,46 @@
 #!/usr/bin/env python
 
 # Main program
+import sqlite3
+import os.path
+from os import path
+import sys
 
-while True:
 
-    barcode = input("Waiting for barcode to be scanned...")
+# Determine if user should be granted access
+def determineAccess(str, c):
+    c.execute('SELECT Name FROM Members WHERE Barcode=?', [str])
+    name = c.fetchone()
 
-    print("Scanned barcode is: " + barcode)
+    if name:
+        print(name[0])
+        return name[0]
+    else:
+        return 0
+    
+# Unlock the door
+def unlockDoor(name):
+    print("Access granted for " + name)
+
+if __name__ == '__main__':
+
+    # Load database for members
+    if path.exists("AccessList.db"):
+        conn = sqlite3.connect('AccessList.db')
+        c = conn.cursor()
+    else:
+        print("ERROR - Did not load database")
+        sys.exit()
+    
+
+    while True:
+
+        barcode = input("Waiting for barcode to be scanned...")
+        print("Scanned barcode is: " + barcode)
+        name = determineAccess(barcode,c)
+        if name:
+            unlockDoor(name)
+        else:
+            print("Access NOT granted.")
+
+
